4508904c8c17c1dae244b743422cc32d5fd3acec
Rahul Baghel <rahulbaghel6211@gmail.com>
2022-10-18 15:34:40 +0530

    update

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Navbar from '../src/Components/Navbar';
+import SignupCard from '../src/Pages/Form';
+
+function App() {
+  return (
+    <div>
+       <Navbar/>
+      <SignupCard/>
+    </div>
+ 
+  );
+}
+
+export default App;
